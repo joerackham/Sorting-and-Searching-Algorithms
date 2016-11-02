18c863167ff0c3d15d2e3d8cc240534cb3eb8954
Joe Rackham <joerackhan@hotmail.co.uk>
2016-11-02 16:25:40 +0000

    Adding2
    Creating an alternative alteration

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.print("Got you now!");
         Sorting sorter = new Sorting();
         Searching searcher = new Searching();
         Integer[] testArray = {14,33,27,10,35,19,42,44};
